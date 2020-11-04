14b3bf9d35cfd2fd8d786b961ec1d602f06bbae5
willowarana <willow@ou.edu>
2020-11-04 17:11:02 -0600

    Set and list unique element comparison

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<String> cats = new ArrayList<>();
+		cats.add("calico");
+		cats.add("persian");
+		cats.add("british shorthair");
+		cats.add("calico");
+		HashSet<String> uniqueCats = new HashSet<>();
+		for(int i = 0; i < cats.size(); ++i) {
+			uniqueCats.add(cats.get(i));
+		}
+		System.out.println(cats);
+		System.out.println(uniqueCats);
 
 	}
 

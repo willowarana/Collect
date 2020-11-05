e71fc3ddc8af09d052b8967f2e74e141fd84bf28
willowarana <willow@ou.edu>
2020-11-04 23:16:14 -0600

    Show difference between HashMap and TreeMap

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.TreeMap;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Show how you would use Set to find the unique elements in a List.
 		ArrayList<String> cats = new ArrayList<>();
 		cats.add("calico");
 		cats.add("persian");
@@ -16,6 +18,30 @@ public class Driver {
 		}
 		System.out.println(cats);
 		System.out.println(uniqueCats);
+		
+		
+		//Show an example of HashMap and a TreeMap, and explain the difference. 
+		HashMap <Character, String> fruits = new HashMap<>();
+		fruits.put('d', "durian");
+		fruits.put('p',"pineapple");
+		fruits.put('a', "apple");
+		fruits.put('c', "coconut");
+		//the fruits will be printed out in unsorted order, because HashMaps are not sorted.
+		System.out.println(fruits);
+		TreeMap<Character, String> sortedFruits = new TreeMap<>();
+		sortedFruits.put('d', "durian");
+		sortedFruits.put('p',"pineapple");
+		sortedFruits.put('a', "apple");
+		sortedFruits.put('c', "coconut");
+		//the fruits will be printed out in sorted order, because TreeMaps are sorted according to the natural ordering of their keys.
+		System.out.println(sortedFruits);
+		
+		//Which should you use (HashMap or TreeMap), if you have to guarantee the order of key, value pairs?
+		
+		//You should use TreeMap because it implements comparable and automatically sorts the pairs according to the natural ordering 
+		//of their keys. 
+		
+		
 
 	}
 
